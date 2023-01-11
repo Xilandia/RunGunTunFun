1fb3a49a2c2d52dc22013a29025394cc0a51fc1d
RoyYotam <95774049+RoyYotam@users.noreply.github.com>
2023-01-11 15:34:59 +0200

    Add files via upload
    The Assets

---
+++ EasyMainMenu/EasyMainMenu/Scripts/Main Menu Scripts/MainMenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    #region Open Different panels
+
+
+    public void newGame()
+    {
+        
+        Debug.Log("Please write a scene name in the 'newGameSceneName' field of the Main Menu Script and don't forget to " +
+                "add that scene in the Build Settings!");
+    }
+    #endregion
+
+    #region Back Buttons
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    #endregion
+
+}
